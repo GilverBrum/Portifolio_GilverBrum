51a40fdecfd5359c878d1d64d3e6522736e62ebd
Gilver <111287036+GilverBrum@users.noreply.github.com>
2024-06-02 20:25:35 -0300

    first commit portifolio

---
+++ app.js
@@ -0,0 +1,54 @@
+function abrirModal() {
+    const modal = document.getElementById('janela-modal')
+    modal.classList.add('abrir')
+
+    modal.addEventListener('click', (e) => {
+        if (e.target.id == 'fecharmodal') {
+            modal.classList.remove('abrir')
+        }
+    })
+}
+
+function abrirModal1() {
+    const modal = document.getElementById('janela-modal1')
+    modal.classList.add('abrir')
+
+    modal.addEventListener('click', (e) => {
+        if (e.target.id == 'fecharmodal') {
+            modal.classList.remove('abrir')
+        }
+    })
+}
+
+function abrirModal2() {
+    const modal = document.getElementById('janela-modal2')
+    modal.classList.add('abrir')
+
+    modal.addEventListener('click', (e) => {
+        if (e.target.id == 'fecharmodal') {
+            modal.classList.remove('abrir')
+        }
+    })
+}
+
+function abrirModal3() {
+    const modal = document.getElementById('janela-modal3')
+    modal.classList.add('abrir')
+
+    modal.addEventListener('click', (e) => {
+        if (e.target.id == 'fecharmodal') {
+            modal.classList.remove('abrir')
+        }
+    })
+}
+
+function abrirModal4() {
+    const modal = document.getElementById('janela-modal4')
+    modal.classList.add('abrir')
+
+    modal.addEventListener('click', (e) => {
+        if (e.target.id == 'fecharmodal') {
+            modal.classList.remove('abrir')
+        }
+    })
+}
